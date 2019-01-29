f747dfa69fc355812e0bb073ed260a8ae250b0a0
Cyril JULLIARD <cjulliar@e1r6p1.42.fr>
2019-01-29 15:37:18 +0100

    29jan19 42 learningPython . Continuation du tuto python, suite a la maison

--- python/learn/cours4/testB/test_random.py
+++ python/learn/cours4/testB/test_random.py
@@ -1,9 +1,8 @@
-#les tests unitaire:
-
 import random
 import unittest
 
 class RandomTest(unittest.TestCase):
+
     """Test case utilisé pour tester les fonctions du module 'random'."""
 
     def setUp(self):
@@ -13,22 +12,14 @@ class RandomTest(unittest.TestCase):
     def test_choice(self):
         """Test le fonctionnement de la fonction 'random.choice'."""
         elt = random.choice(self.liste)
-        # Vérifie que 'elt' est dans 'liste'
         self.assertIn(elt, self.liste)
 
-#    def test_choiceFalse(self):
-#        """Test le fonctionnement de la fonction 'random.choice'."""
-#        elt = random.choice(self.liste)
-#        self.assertIn(elt, ('a', 'b', 'c'))
-
-    # Autres méthodes de test
     def test_shuffle(self):
         """Test le fonctionnement de la fonction 'random.shuffle'."""
         random.shuffle(self.liste)
         self.liste.sort()
         self.assertEqual(self.liste, list(range(10)))
 
-	# Autres méthodes de test
     def test_sample(self):
         """Test le fonctionnement de la fonction 'random.sample'."""
         extrait = random.sample(self.liste, 5)
@@ -38,9 +29,5 @@ class RandomTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             random.sample(self.liste, 20)
 
-
-#run le test unitaire:
-print(unittest.main())
-
-
-
+# Pour tester tous les tests unitaire du fichier, on tape la commande:
+# python3.6 -m unittest
---
+++ python/learn/cours4/testD.py
@@ -0,0 +1,41 @@
+#plusieurs threads mais bloqué par un verou
+
+import random
+import sys
+from threading import Thread, RLock
+import time
+
+verrou = RLock()
+
+class Afficheur(Thread):
+
+    """Thread chargé simplement d'afficher un mot dans la console."""
+
+    def __init__(self, mot):
+        Thread.__init__(self)
+        self.mot = mot
+
+    def run(self):
+        """Code à exécuter pendant l'exécution du thread."""
+        i = 0
+        while i < 9:
+            with verrou:
+                for lettre in self.mot:
+                    sys.stdout.write(lettre)
+                    sys.stdout.flush()
+                    attente = 0.02
+                    attente += random.randint(1, 60) / 1000
+                    time.sleep(attente)
+            i += 1
+
+# Création des threads
+thread_1 = Afficheur("canard")
+thread_2 = Afficheur("TORTUE")
+
+# Lancement des threads
+thread_1.start()
+thread_2.start()
+
+# Attend que les threads se terminent
+thread_1.join()
+thread_2.join()
