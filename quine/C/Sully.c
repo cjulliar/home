3674c66edf09002acfd4bd3a0a0e04665517d534
Cyril J <cjulliar@e1r8p21.42.fr>
2019-07-12 02:31:40 +0200

    12juil 42 quine . quine c part fini

--- quine/C/Sully.c
+++ quine/C/Sully.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define entier 7
+
 int main()
 {
+	int entier = 5;
+	if(entier < 0)
+		return (0);
+	char*name="Sully_%d.c";
+	char *exec="clang -Wall -Wextra -Werror %s -o Sully_%d ; ./Sully_%d";
 	char *file;
-	asprintf(&file, "Sully_%d.c", entier);
+	asprintf(&file, name, entier);
 	FILE *f = NULL;
 	f=fopen(file,"w");
 	if(f != NULL)
 	{
-		char *a="#include <stdio.h>%c#include <stdlib.h>%c#define entier %d%cint main()%c{%cchar *file;%casprintf(&file, %cSully_%d.c%c);%cFILE *f = NULL;%cf=fopen(file,%cw%c);%cif(f != NULL)%c{%cchar *a=%c%s%c%c;fprintf(f,a,10,10,entier - 1,10,10,10,10, 34,entier - 1,34,10, 10, 34,34,10, 10,10, 34,a,34,10, 10,10,10,10,10,10, 34,entier - 1,34,10, 10,10);%cfclose(f);%c}%cif (entier > 1)%c{%cchar *make;%casprintf(&make, %cclang -Wall -Wextra -Werror Sully_%d.c -o Sully ; ./Sully%c);%csystem(make);%c}%c}";
-		fprintf(f,a,10,10,entier - 1,10,10,10,10, 34,entier - 1,34,10, 10, 34,34,10, 10,10, 34,a,34,10, 10,10,10,10,10,10, 34,entier - 1,34,10, 10,10);
+		char *a="#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$cint main()%1$c{%1$c%7$cint entier = %2$d;%1$c%7$cif(entier < 0)%1$c%7$c%7$creturn (0);%1$c%7$cchar*name=%6$c%3$s%6$c;%1$c%7$cchar *exec=%6$c%4$s%6$c;%1$c%7$cchar *file;%1$c%7$casprintf(&file, name, entier);%1$c%7$cFILE *f = NULL;%1$c%7$cf=fopen(file,%6$cw%6$c);%1$c%7$cif(f != NULL)%1$c%7$c{%1$c%7$c%7$cchar *a=%6$c%5$s%6$c;%1$c%7$c%7$cfprintf(f,a,10, entier-1, name, exec, a, 34, 9);%1$c%7$c%7$cfclose(f);%1$c%7$c}%1$c%7$cchar *make;%1$c%7$casprintf(&make, exec, file, entier, entier);%1$c%7$csystem(make);%1$c%7$c}%1$c";
+		fprintf(f,a,10, entier-1, name, exec, a, 34, 9);
 		fclose(f);
 	}
-	if (entier > 1)
-	{
-		char *make;
-		asprintf(&make, "clang -Wall -Wextra -Werror %s -o Sully ; ./Sully", file);
-		system(make);
-	}
+	char *make;
+	asprintf(&make, exec, file, entier, entier);
+	system(make);
 }
